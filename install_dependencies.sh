51529f98c60f8c35b6d65263aa7bcd30f608ff47
renn0xtek9 <renn0xtek9@laposte.net>
2018-11-11 20:27:34 +0100

    Add eslint for static code analysis

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -6,4 +6,6 @@ sudo npm install acorn
 sudo npm install mysql
 sudo npm install walk
 sudo apt-get install mysql-server
-
+sudo npm install node-id3
+sudo npm install eslint --save-dev
+./node_modules/.bin/eslint --init
--- validatejavascripts.sh
+++ validatejavascripts.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 for f in $(find -name "*.js")
 do 
+	if [[ "$f" == *"node_modules"* ]]; then   #eg if [[ $str == *"in"* ]]
+		continue
+	fi
 	echo $f
 	acorn --silent $f
+	
+	./node_modules/.bin/eslint $f.js
+	
 done 
